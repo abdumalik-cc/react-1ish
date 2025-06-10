327d56b02d184c314aa3c1bd5c3c9845bd03c36f
abdumalik-cc <rakhmatovabdumalik5@gmail.com>
2025-06-10 15:54:42 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1>Abdumalik</h1>
+       <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique enim perspiciatis sequi expedita. Dicta laborum architecto odio repellendus in inventore veniam minima est deserunt maiores aperiam doloribus rerum maxime pariatur modi laudantium, quos necessitatibus, optio, dolores odit accusamus quae asperiores. Molestiae, amet nesciunt fugiat excepturi ratione, ab velit cum ipsa voluptatibus eaque, laudantium saepe perspiciatis.</p>
+       <ul>
+           <li>Html</li>
+           <li>Css</li>
+           <li>React</li>
+           <li>JavaScript</li>
+       </ul>
+    </div>
+  )
+}
+
+export default App
